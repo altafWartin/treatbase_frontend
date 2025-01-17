bf409272762bc563fc69ff09490a4cca0cdbdbe6
altafWartin <altaf@wartinlabs.com>
2025-01-17 18:21:12 +0530

    update code

--- src/components/PatientDetails copy.jsx
+++ src/components/PatientDetails copy.jsx
@@ -12,7 +12,7 @@ export default function PatientDetails() {
         firstName: "",
         lastName: "",
         date: new Date(),
-        dateOfBirth: "",
+        dateOfBirth: "",           
         age: "",
         gender: "",
         preOp: false,
